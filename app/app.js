7f44db2755168322c25cc3482b492582befdc447
blundercode <erik.mcland@gmail.com>
2015-08-05 20:59:16 -0600

    first commit

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// var app = angular.module('jibbirApp', ['ngRoute']);
+
+// app.config(function ($routeProvider) {
+//   $routeProvider
+//     .when('/home', {
+//       templateUrl: 'app/homeTmplt.html',
+//       controller: 'homeCtrl'
+//     })
+//     .otherwise({
+//       redirectTo: '/home'
+//     })
+// });
